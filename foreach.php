6140b7ae026aaaafc98be30fcadcf9df9033e03f
ashish aggarwal <ashish09101998@gmail.com>
2017-12-11 20:09:42 +0530

    some basics
    loops,ifstatement,mathfile

---
+++ foreach.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$numbers=array(345,345,45,67,78,69) ;
+foreach($numbers as $number)
+{
+echo $number."<br>" ;
+}
+?>
+</body>
+</html>
---
+++ maths.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+echo pow(3,5)."<br>" ;
+echo rand(1,100) ;     
+?>
+</body>
+</html>
