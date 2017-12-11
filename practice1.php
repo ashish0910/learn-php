a3d0eef378839efb3cee97c19166f274487f909a
ashish aggarwal <ashish09101998@gmail.com>
2017-12-11 20:08:27 +0530

    arrays_assoc

---
+++ practice1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$numberList= array(1,2,3,4,"888","<h1>hell world</h1>") ;
+    
+print_r($numberList) ;    
+    
+?>    
+</body>
+</html>
