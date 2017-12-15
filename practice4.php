d48b9662915982bdcbed176b992cddb7f2ae1043
ashish aggarwal <30404493+ashish0910@users.noreply.github.com>
2017-12-15 12:47:09 +0530

    Add files via upload
    some practices.

---
+++ practice4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function sum($n1,$n2)
+{
+return $n1+$n2 ;
+}
+echo sum(5,7)."<br>" ;
+define("h",20) ;
+echo "<br>".h ;    
+    
+    
+?>
+</body>
+</html>
