f8ab826d5daa731fd8af839ff8f72d312e7899d2
ashish aggarwal <ashish09101998@gmail.com>
2017-12-15 15:34:15 +0530

    cookies
    how to set up cookies

---
+++ cookies.php
@@ -0,0 +1,21 @@
+<?php
+$name= "SomeName" ;
+$value= 100 ;
+$expiration= time() + (60*60*24*7) ;
+
+
+setcookie($name,$value,$experiration) ;
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
