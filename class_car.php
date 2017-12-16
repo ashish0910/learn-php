7afe7c83736de5bacc5badb248d8467d99aa5ab4
ashish aggarwal <ashish09101998@gmail.com>
2017-12-16 11:20:11 +0530

    classes
    syntax of a class.

---
+++ class_car.php
@@ -0,0 +1,29 @@
+<?php
+
+class Car {
+
+    
+    
+    
+}
+
+if(class_exists("Car"))
+{
+    echo "exists" ;
+}
+else
+echo "does not exist" ;    
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
